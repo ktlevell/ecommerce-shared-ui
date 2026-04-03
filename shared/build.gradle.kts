696167037f2d3b3e1a2674e760b02036a8f3c0a9
Jhônatan Santana <jhonatansantanati@gmail.com>
2026-04-02 23:11:00 -0300

    chore: config build.gradle to shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,39 @@
 plugins {
-    "kotlin"
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
 }
 
-dependencies {
+kotlin {
+    androidTarget()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "Shared"
+            isStatic = true
+        }
+    }
 
+    sourceSets {
+        commonMain.dependencies {
+
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.ktlevell.ecommercesharedui"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
 }
